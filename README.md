3f464144339e5af9dea3ac9a984c0690aaa0af57
windsail <windsailg@gmail.com>
2020-08-26 17:15:28 +0800

    create restaurant_list project

---
+++ README.md
@@ -0,0 +1,48 @@
+# 我的餐廳口袋名單
+
+本清單提供餐廳的類型搜尋、查看餐廳照片以及地理位置等資訊
+
+## 現有功能
+- 列出所有餐廳資訊
+- 可以依照餐廳名稱及餐廳類別做搜尋
+- 點選資訊卡可直接檢視餐廳詳細資訊，包含類別、地址、電話、評分、圖片及 Google Map
+    - 點選 Google Map`顯示詳細地圖`可查看位置詳細資料
+
+
+### 安裝
+1.開啟終端機(Terminal) cd 到存放專案本機位置並執行:
+
+```
+git clone https://github.com/windsailg/ac_restaurant_list.git
+```
+
+
+2.初始化安裝套件
+
+```
+cd ac_restaurant_list  //切至專案資料夾
+```
+
+```
+npm install  //安裝套件
+```
+
+```
+npm run dev  //啟動程式
+```
+
+
+3.Terminal 顯示以下資訊
+
+```
+Express is listening on localhost:3000 
+//伺服器已經成功運作於 http://localhost:3000
+```
+
+
+
+## 使用工具
+Node.js: 13.0.1
+Express: 4.17.1
+Express-Handlebars: 5.1.0
+nodemon: 2.0.4
---
+++ app.js
@@ -0,0 +1,40 @@
+//include express
+const express = require('express')
+const expHandlebars = require('express-handlebars')
+const app = express()
+const restaurants= require('./restaurant.json')
+
+//define server related variables
+const port = 3000
+
+app.engine('handlebars', expHandlebars({
+    defaultLayout: 'main'
+}))
+
+app.set('view engine', 'handlebars')//set view engine
+
+app.use(express.static('public'))
+
+app.get('/',(req , res)=>{
+    res.render('index',{restaurant:restaurants.results})
+})
+
+app.get('/restaurant/:restaurant_id',(req , res)=>{
+    const currentRestaurant = restaurants.results.find(restaurants=> restaurants.id ===Number(req.params.restaurant_id))
+    res.render('show',{restaurant:currentRestaurant})
+})
+
+app.get('/search',(req , res)=>{
+    const keyword = req.query.keyword.trim().toLowerCase()
+    const searchedRestaurant = restaurants.results.filter(ele=>{
+            return ele.name.toLowerCase().includes(keyword) || ele.category.toLowerCase().includes(keyword)
+    })
+    res.render('index',{restaurant:searchedRestaurant,keywords:keyword})
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Express is running on http://localhost:${port}`)
+})
