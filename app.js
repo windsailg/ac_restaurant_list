c24b5d134a6f7b3f9fef45ae01c30f119a717e41
windsail <windsailg@gmail.com>
2020-08-28 02:27:16 +0800

    edit view page code and use JS Standard style

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
 //include express
-const express = require('express')
-const expHandlebars = require('express-handlebars')
-const app = express()
-const restaurants= require('./restaurant.json')
+const express = require ('express')
+const expHandlebars = require ('express-handlebars')
+const app = express ()
+const restaurants = require ('./restaurant.json')
 
 //define server related variables
 const port = 3000
 
-app.engine('handlebars', expHandlebars({
+app.engine ('handlebars', expHandlebars({
     defaultLayout: 'main'
 }))
 
-app.set('view engine', 'handlebars')//set view engine
+app.set ('view engine', 'handlebars')//set view engine
 
-app.use(express.static('public'))
+app.use (express.static('public'))
 
-app.get('/',(req , res)=>{
-    res.render('index',{restaurant:restaurants.results})
+app.get ('/', (req , res)=>{
+    res.render ('index',{restaurant:restaurants.results})
 })
 
-app.get('/restaurant/:restaurant_id',(req , res)=>{
-    const currentRestaurant = restaurants.results.find(restaurants=> restaurants.id ===Number(req.params.restaurant_id))
-    res.render('show',{restaurant:currentRestaurant})
+app.get ('/restaurant/:restaurant_id',(req , res)=>{
+    const currentRestaurant = restaurants.results.find(restaurants => restaurants.id === Number(req.params.restaurant_id))
+    res.render ('show',{restaurant:currentRestaurant})
 })
 
-app.get('/search',(req , res)=>{
+app.get ('/search',(req , res)=>{
     const keyword = req.query.keyword.trim().toLowerCase()
-    const searchedRestaurant = restaurants.results.filter(ele=>{
-            return ele.name.toLowerCase().includes(keyword) || ele.category.toLowerCase().includes(keyword)
+    const searchedRestaurant = restaurants.results.filter(ele => {
+        return ele.name.toLowerCase().includes(keyword) || ele.category.toLowerCase().includes(keyword)
     })
-    res.render('index',{restaurant:searchedRestaurant,keywords:keyword})
+    res.render ('index', {restaurant: searchedRestaurant, keywords: keyword})
 })
 
-
-
-
-app.listen(port,()=>{
-    console.log(`Express is running on http://localhost:${port}`)
+app.listen (port , () =>{
+    console.log('Express is running on http://localhost:' + 'port')
 })
